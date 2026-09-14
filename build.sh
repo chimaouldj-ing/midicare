f71a3dcdc968a2902b2749538338103d98b311fe
Chima Ouldj <chimaouldj@gmail.com>
2026-09-14 07:47:31 +0200

    Seed doctor for deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell <<'PY'
+from accounts.models import User, Doctor, Speciality
+
+user, created = User.objects.get_or_create(
+    username="doctor1",
+    defaults={
+        "first_name": "Doctor",
+        "last_name": "Test",
+    }
+)
+
+user.set_password("Doctor12345")
+user.save()
+
+speciality, _ = Speciality.objects.get_or_create(name="Cardiology")
+
+Doctor.objects.get_or_create(
+    user=user,
+    defaults={
+        "speciality": speciality,
+        "phone": "0550000000",
+    }
+)
+PY
